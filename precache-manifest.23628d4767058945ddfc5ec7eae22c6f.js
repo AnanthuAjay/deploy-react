7fe71a62d4e5c5d507146cc9de9f476f16391c5b
Ananthu Ajay <andw.aj.2k@gmail.com>
2020-09-29 20:41:47 +0530

    Updates

---
+++ precache-manifest.23628d4767058945ddfc5ec7eae22c6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3293450ee0119ddb6e316785d0aa67",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "69691b6fb5a267acd7f7",
+    "url": "/deploy-react/static/css/2.2f73783b.chunk.css"
+  },
+  {
+    "revision": "20ea67378bd21b364ac2",
+    "url": "/deploy-react/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "69691b6fb5a267acd7f7",
+    "url": "/deploy-react/static/js/2.80c48d67.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/deploy-react/static/js/2.80c48d67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20ea67378bd21b364ac2",
+    "url": "/deploy-react/static/js/main.76812336.chunk.js"
+  },
+  {
+    "revision": "45a0c3bc7a9ff3e9bc8e",
+    "url": "/deploy-react/static/js/runtime-main.44953e47.js"
+  }
+]);
